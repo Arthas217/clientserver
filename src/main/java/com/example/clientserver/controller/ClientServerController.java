5ae26c05bca423bd8008326badfb3afbc9a9fa57
zuocheng <442854399@qq.com>
2022-07-26 18:37:08 +0800

    init

--- src/main/java/com/example/clientserver/controller/ClientServerController.java
+++ src/main/java/com/example/clientserver/controller/ClientServerController.java
@@ -1,8 +1,7 @@
 package com.example.clientserver.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author 会游泳的蚂蚁
@@ -12,10 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClientServerController {
 
-    @RequestMapping("/m1")
-    public void method1(@RequestParam(value = "token") String token) {
-        System.out.println("111111---" + token);
-    }
+
     // 一、 @RequestMapping 支持get
     //  required = true 默认
     //  1、不传参数token
@@ -36,6 +32,35 @@ public class ClientServerController {
     //  Content-Type为application/json
     //  请求响应：{"timestamp":"2022-07-26T09:49:56.661+00:00","status":400,"error":"Bad Request","path":"/m1"}
     //  后端报错org.springframework.web.bind.MissingServletRequestParameterException: request parameter 'token' for method parameter type String is not present]
+
+    @RequestMapping("/m1")
+    public void method1(@RequestParam(value = "token") String token) {
+        System.out.println("111111---" + token);
+    }
+
+    // 二、@RequestBody  接收的参数是来自requestBody中，即请求体
+    // 1、get请求
+    // -浏览器请求   失败org.springframework.http.converter.HttpMessageNotReadableException: Required request body is missing: public void com.example.clientserver.controller.ClientServerController.method3(java.lang.String)]
+    // -postman请求  Content-Type为application/x-www-form-urlencoded   success
+    //  Content-Type为application/json   后端直接返回 222222---{"token":"2"}
+
+    // 2、post请求
+    // -postman请求  Content-Type为application/x-www-form-urlencoded   success
+
+    @RequestMapping("/m2")
+    public void method2(@RequestBody String token) {
+        System.out.println("222222---" + token);
+    }
+
+
+    // 三、 @ResponseBody
+
+    @RequestMapping("/m3")
+    @ResponseBody
+    public JSONObject method3(@RequestBody JSONObject token) {
+       return token;
+    }
+
 
 
 
